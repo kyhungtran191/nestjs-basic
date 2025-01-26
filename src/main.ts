424807f20dd4a501beecd22d0eb22b678b5fd961
justinJSwebdev <trankyhung225@gmail.com>
2025-01-26 12:34:39 +0700

    configure env

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { ConfigService } from '@nestjs/config';
 // Start app by bootstrap
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Get ConfigService
+  const configService = app.get(ConfigService);
+  // Get env variable
+  const PORT = configService.get('PORT');
+  await app.listen(PORT || 3000);
 }
 bootstrap();
